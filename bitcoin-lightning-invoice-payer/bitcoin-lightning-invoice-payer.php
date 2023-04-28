63ad243f1dcd316a00e80bfb57409eab7c4bf483
Erik Alvarez <erik@velascommerce.com>
2023-04-28 09:48:44 -0400

    Updated api-key, nonce and sanitize functions

--- bitcoin-lightning-invoice-payer/bitcoin-lightning-invoice-payer.php
+++ bitcoin-lightning-invoice-payer/bitcoin-lightning-invoice-payer.php
@@ -1,4 +1,5 @@
- <?php
+<?php
+require_once 'api_key.php'; // Include the api_key.php file
 /*
  * Plugin Name:       LNBits Bitcoin Lightning Network Invoice Payer
  * Plugin URI:        https://www.velascommerce.com/
@@ -36,15 +37,25 @@ function my_enqueue() {
  wp_enqueue_script('bitcoin_data', get_template_directory_uri().'/js/post-like.js', '1.0', 1 );
  wp_localize_script('bitcoin_data', 'ajax_var', array(
  'ajax_url' => admin_url('admin-ajax.php'),
- 'nonce' => wp_create_nonce('ajaxnonce'),
+ 'nonce' => wp_create_nonce('invoice_payment_nonce'),
  'loader_img' => plugin_dir_url( __FILE__ ) . 'img/loader.gif' 
  ));
 }
 
 function invoice_payment() {
-    $string = $_POST['string'];
-    $data_array =  array( "bolt11" => $string );
-    $decode_data_array =  array( "data" => $string );
+   if (!isset($_POST['nonce']) || !wp_verify_nonce($_POST['nonce'], 'invoice_payment_nonce')) { // Update the nonce name here
+      echo json_encode(array("status" => 'error', "error" => "Invalid nonce."));
+      die();
+    }
+
+   $string = isset($_POST['string']) ? sanitize_text_field($_POST['string']) : '';
+
+   if ($string === '') {
+       echo json_encode(array("status"=>'error', "error"=>"Invoice string is empty."));
+       die();
+   }
+   $data_array =  array( "bolt11" => $string );
+   $decode_data_array =  array( "data" => $string );
     $decode_data = callAPI('POST', 'https://legend.lnbits.com/api/v1/payments/decode', json_encode($decode_data_array));
     $response = json_decode($decode_data, true);
      if(!empty($response)){
@@ -107,7 +118,7 @@ function callAPI($method, $url, $data){
    // OPTIONS:
    curl_setopt($curl, CURLOPT_URL, $url);
    curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-      'X-Api-Key: '.LNbits_API_KEY,
+      'X-Api-Key: ' . LNbits_API_KEY, // Use the constant instead of the hardcoded API key
       'Content-Type: application/json',
       'Content-length:' . strlen($data)
    ));
--- bitcoin-lightning-invoice-payer/js/custom.js
+++ bitcoin-lightning-invoice-payer/js/custom.js
@@ -1,25 +1,33 @@
-jQuery(document).ready(function(){
-	jQuery(document).on("click","button#submit-invoice-string",function(){
-		var get_invoice_string = jQuery("#msf-text-paste-your-invoice-here").val();
-		if(get_invoice_string!=""){
-			//alert("testing------------"+ajax_var.ajax_url);
-			jQuery(".message-data").empty().html('<img src="'+ajax_var.loader_img+'">');
-				 jQuery.ajax({
-	             type : "POST",
-	             dataType : "json",
-	             url : ajax_var.ajax_url,
-	             data : {"action": "invoice_payment","string":get_invoice_string},
-	             success: function(response) {
-	             	//console.log(response);
-		             	if(response.status=="true"){
-		             		jQuery(".message-data").html('<span class="success">You have recieved the payment. Please check your wallet</span>');
-		             	}else{
-		             		jQuery(".message-data").html('<span class="error">'+response.error+'</span>');
-		             	}
-	                }
-	        });
-		}
-		
-
-	})
-})
+jQuery(document).ready(function () {
+  jQuery(document).on("click", "button#submit-invoice-string", function () {
+    var get_invoice_string = jQuery("#msf-text-paste-your-invoice-here").val();
+    if (get_invoice_string != "") {
+      //alert("testing------------"+ajax_var.ajax_url);
+      jQuery(".message-data")
+        .empty()
+        .html('<img src="' + ajax_var.loader_img + '">');
+      jQuery.ajax({
+        type: "POST",
+        dataType: "json",
+        url: ajax_var.ajax_url,
+        data: {
+          action: "invoice_payment",
+          string: get_invoice_string,
+          nonce: ajax_var.nonce, // Include the nonce value within the data object
+        },
+        success: function (response) {
+          //console.log(response);
+          if (response.status == "true") {
+            jQuery(".message-data").html(
+              '<span class="success">You have received the payment. Please check your wallet</span>'
+            );
+          } else {
+            jQuery(".message-data").html(
+              '<span class="error">' + response.error + "</span>"
+            );
+          }
+        },
+      });
+    }
+  });
+});
