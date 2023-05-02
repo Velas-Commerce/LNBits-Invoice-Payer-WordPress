f36217d81a0355a51e00bd771900959606da31ad
Erik Alvarez <erik@velascommerce.com>
2023-05-02 09:09:50 -0400

    added ip-checks and set cookies

--- bitcoin-lightning-invoice-payer/bitcoin-lightning-invoice-payer.php
+++ bitcoin-lightning-invoice-payer/bitcoin-lightning-invoice-payer.php
@@ -2,9 +2,9 @@
 require_once 'api_key.php'; // Include the api_key.php file
 /*
  * Plugin Name:       LNBits Bitcoin Lightning Network Invoice Payer
- * Plugin URI:        https://www.velascommerce.com/
+ * Plugin URI:        https://github.com/Velas-Commerce/LNBits-Invoice-Payer-WordPress/
  * Description:       Add Bitcoin rewards to your wordpress site by programmatically paying bolt 11 Lightning invoices using a low-risk LNBits wallet and API key.
- * Version:           1.10.3
+ * Version:           1.2.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            VelasCommerce
@@ -58,9 +58,16 @@ function invoice_payment() {
    $decode_data_array =  array( "data" => $string );
     $decode_data = callAPI('POST', 'https://legend.lnbits.com/api/v1/payments/decode', json_encode($decode_data_array));
     $response = json_decode($decode_data, true);
+
+    global $wpdb;
+    $table_name = $wpdb->prefix.'payment_records';
+    $get_ip = getIPAddress();  
+	
+	$query = "SELECT * FROM  ".$table_name." WHERE ip_addr = '".$get_ip."'";
+	$results = $wpdb->get_results($query);
+
      if(!empty($response)){
           if(isset($response['amount_msat']) && $response['amount_msat']!="" ){
-          //  print_r($response);die;
             $total_asking_amount = $response['amount_msat']/1000;
             if($total_asking_amount>100){
              echo json_encode(array("status"=>'error',"error"=>"Please do not raise invoice for more than 100 Satoshis.")); 
@@ -72,33 +79,66 @@ function invoice_payment() {
          }
      }
 
-    $get_data = callAPI('POST', 'https://legend.lnbits.com/api/v1/payments', json_encode($data_array));
-    $response = json_decode($get_data, true);
-    
-    if(!empty($response)){
-     // print_r($response);die;
-      if(isset($response['payment_hash']) && $response['payment_hash']!="" ){
-       //  print_r($response);die;
-          echo json_encode(array("status"=>'true',"payment_hash"=>$response['payment_hash'])); 
-      }elseif(isset($response['detail']) && $response['detail']!=""){
-        // print_r($response);die('sudddddddddddddddddddddddddddd');
-         echo json_encode(array("status"=>'false',"error"=>"Either API is down or string has been already paid."));  
-     
-    }else{
-         echo json_encode(array("status"=>'false',"error"=>"Please contact tech team.")); 
+      if(empty($results)){
+             $get_data = callAPI('POST', 'https://legend.lnbits.com/api/v1/payments', json_encode($data_array));
+             $response = json_decode($get_data, true);       
+             if(!empty($response)){
+               if(isset($response['payment_hash']) && $response['payment_hash']!="" ){
+                  $wpdb->insert($table_name, array(
+                      'ip_addr' => $get_ip
+                  ));
+                   setcookie("btc_payment_completed", 1, time()+86400);  /* expire in 24 hours */
+                   echo json_encode(array("status"=>'true',"payment_hash"=>$response['payment_hash'])); 
 
-    }
-    die();
-   // print_r($response);die;
+               }elseif(isset($response['detail']) && $response['detail']!=""){
+                  echo json_encode(array("status"=>'false',"error"=>"Either API is down or string has been already paid."));  
+              
+             }else{
+                  echo json_encode(array("status"=>'false',"error"=>"Please contact tech team.")); 
 
-}
+             }
+             die();
+
+         }
+      }else{
+
+            if(isset($_COOKIE['btc_payment_completed'])) {
+               echo json_encode(array("status"=>'error',"error"=>"Please wait for 24 hours from the time you have received the last payment.")); 
+                   die();exit;
+            }else {
+
+                $get_data = callAPI('POST', 'https://legend.lnbits.com/api/v1/payments', json_encode($data_array));
+                   $response = json_decode($get_data, true);       
+                   if(!empty($response)){
+                     if(isset($response['payment_hash']) && $response['payment_hash']!="" ){
+                        $wpdb->insert($table_name, array(
+                            'ip_addr' => $get_ip
+                        ));
+                         setcookie("btc_payment_completed", 1, time()+86400);  /* expire in 24 hours */
+                         echo json_encode(array("status"=>'true',"payment_hash"=>$response['payment_hash'])); 
+
+                     }elseif(isset($response['detail']) && $response['detail']!=""){
+                        echo json_encode(array("status"=>'false',"error"=>"Either API is down or string has been already paid."));  
+                    
+                   }else{
+                        echo json_encode(array("status"=>'false',"error"=>"Please contact tech team.")); 
+
+                   }
+                   die();
+
+               }
+
+            }
+ 
+                   die();exit;
+      }
+
 }
 
 add_action( 'wp_ajax_nopriv_invoice_payment', 'invoice_payment' );
 add_action( 'wp_ajax_invoice_payment', 'invoice_payment' );
 
 function callAPI($method, $url, $data){
-   // print_r($data);die;
    $curl = curl_init();
    switch ($method){
       case "POST":
@@ -129,4 +169,46 @@ function callAPI($method, $url, $data){
    if(!$result){die("Connection Failure");}
    curl_close($curl);
    return $result;
+}
+/*create tables on plugin initialization for having records */
+
+global $btc_db_version;
+$btc_db_version = '1.0';
+
+function btc_table_installation() {
+   global $wpdb;
+   global $btc_db_version;
+
+   $table_name = $wpdb->prefix . 'payment_records';
+   
+   $charset_collate = $wpdb->get_charset_collate();
+
+   $sql = "CREATE TABLE $table_name (
+      id mediumint(9) NOT NULL AUTO_INCREMENT,
+      time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+      ip_addr varchar(100) DEFAULT '' NOT NULL,
+      PRIMARY KEY  (id)
+   ) $charset_collate;";
+
+   require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+   dbDelta( $sql );
+
+   add_option( 'btc_db_version', $btc_db_version );
+}
+register_activation_hook( __FILE__, 'btc_table_installation' );
+/*Fetch real IP adress*/
+ function getIPAddress() {  
+    //whether ip is from the share internet  
+     if(!empty($_SERVER['HTTP_CLIENT_IP'])) {  
+                $ip = $_SERVER['HTTP_CLIENT_IP'];  
+        }  
+    //whether ip is from the proxy  
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
+                $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];  
+     }  
+//whether ip is from the remote address  
+    else{  
+             $ip = $_SERVER['REMOTE_ADDR'];  
+     }  
+     return $ip;  
 }
